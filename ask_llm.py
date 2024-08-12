60f4e649d60f873829297b9f984f5dec2d768136
redavis22 <redavis22@gmail.com>
2024-08-12 18:25:43 -0400

    Update ask_llm.py

--- ask_llm.py
+++ ask_llm.py
@@ -10,24 +10,14 @@ client = OpenAI(
 )
 
 
-while(True):
-    response = client.chat.completions.create(
-      model="gpt-4o",
-      messages=[
-        {
-          "role": "user",
-          "content": [
-            {"type": "text", "text": prompt},
-            # {
-            #   "type": "image_url",
-            #   "image_url": {
-            #     "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-            #   },
-            # },
-          ],
-        }
-      ],
-      max_tokens=300,
-    )
-    
-    print(response.choices[0].message.content)
+
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    "role": "system", "content": "GIVE ONE LINE BASH COMMAND AS YOUR ANSWER. BASH FORMAT ONLY",
+    "role": "user", "content": prompt
+  ],
+  max_tokens=300,
+)
+
+print(response.choices[0].message.content)
